f24e80cdc0ecab799f04b236bda6b3df5ab50f96
muffinFactory <muhharismaulana@gmail.com>
2025-08-12 16:33:36 +0700

    feat: implement background theme

--- src/components/Layout/BaseScreen.tsx
+++ src/components/Layout/BaseScreen.tsx
@@ -2,16 +2,29 @@ import React from "react"
 import { StyleProp, StyleSheet, View, ViewStyle } from "react-native"
 
 import { useBottomWindowPadding } from "src/hooks/layout/useBottomWindowPadding"
+import { useAppTheme } from "src/hooks/useAppTheming"
 
 type Props = {
   style?: StyleProp<ViewStyle>
   children?: React.ReactNode
+  fullscreen?: boolean
 }
 
-const BaseScreen = ({ style, children }: Props) => {
+const BaseScreen = ({ style, children, fullscreen }: Props) => {
   const bottomSpace = useBottomWindowPadding()
+  const Theme = useAppTheme()
 
-  return <View style={[styles.container, { paddingBottom: bottomSpace }, style]}>{children}</View>
+  return (
+    <View
+      style={[
+        styles.container,
+        { paddingBottom: fullscreen ? undefined : bottomSpace, backgroundColor: Theme.background },
+        style
+      ]}
+    >
+      {children}
+    </View>
+  )
 }
 
 export default BaseScreen
--- src/components/Layout/BaseScrollViewScreen.tsx
+++ src/components/Layout/BaseScrollViewScreen.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { ScrollView, StyleProp, StyleSheet, ViewStyle } from "react-native"
 
 import { useBottomWindowPadding } from "src/hooks/layout/useBottomWindowPadding"
+import { useAppTheme } from "src/hooks/useAppTheming"
 
 type Props = {
   style?: StyleProp<ViewStyle>
@@ -10,8 +11,13 @@ type Props = {
 
 const BaseScrollViewScreen = ({ style, children }: Props) => {
   const bottomSpace = useBottomWindowPadding()
+  const Theme = useAppTheme()
 
-  return <ScrollView style={[styles.container, { paddingBottom: bottomSpace }, style]}>{children}</ScrollView>
+  return (
+    <ScrollView style={[styles.container, { paddingBottom: bottomSpace, backgroundColor: Theme.primary }, style]}>
+      {children}
+    </ScrollView>
+  )
 }
 
 export default BaseScrollViewScreen
