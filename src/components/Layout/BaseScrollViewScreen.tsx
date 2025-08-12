e58a837c43c4764a683485a527cd15e4df89c945
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 00:18:09 +0700

    feat: BaseScrollView fullscreen support

--- src/components/Layout/BaseScrollViewScreen.tsx
+++ src/components/Layout/BaseScrollViewScreen.tsx
@@ -1,5 +1,7 @@
 import React from "react"
-import { ScrollView, StyleProp, StyleSheet, ViewStyle } from "react-native"
+import { ScrollView, StyleProp, StyleSheet, View, ViewStyle } from "react-native"
+
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { useBottomWindowPadding } from "src/hooks/layout/useBottomWindowPadding"
 import { useAppTheme } from "src/hooks/useAppTheming"
@@ -7,16 +9,21 @@ import { useAppTheme } from "src/hooks/useAppTheming"
 type Props = {
   style?: StyleProp<ViewStyle>
   children?: React.ReactNode
+  fullscreen?: boolean
 }
 
-const BaseScrollViewScreen = ({ style, children }: Props) => {
+const BaseScrollViewScreen = ({ style, children, fullscreen }: Props) => {
   const bottomSpace = useBottomWindowPadding()
+  const insets = useSafeAreaInsets()
   const Theme = useAppTheme()
 
   return (
-    <ScrollView style={[styles.container, { paddingBottom: bottomSpace, backgroundColor: Theme.primary }, style]}>
-      {children}
-    </ScrollView>
+    <View style={styles.container}>
+      {!fullscreen && <View style={{ height: insets.top, width: "100%", backgroundColor: Theme.statusBarColor }} />}
+      <ScrollView style={[styles.container, { paddingBottom: bottomSpace, backgroundColor: Theme.background }, style]}>
+        {children}
+      </ScrollView>
+    </View>
   )
 }
 
