8a5fca832fa99678fd65957a67af5ce382af28a5
muffinFactory <muhharismaulana@gmail.com>
2025-08-12 22:49:39 +0700

    feat: isGuest check

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,7 +1,9 @@
 import { LocationEventType } from "./event"
 
+// Todo: expand this
 export type User = {
   name: string
+  isGuest?: boolean
   favorite_events_ids?: LocationEventType["id"][]
   favorite_events?: LocationEventType[] // initial lazily
 }
