52d5dfafe7628177e2aaa23c6f88bb4da6601020
muffinFactory <muhharismaulana@gmail.com>
2025-08-12 11:42:18 +0700

    chore(dev): eslint deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ["@react-native", "plugin:react/recommended", "plugin:react/jsx-runtime", "prettier"],
+  plugins: ["import", "prettier"],
+  rules: {
+    "prettier/prettier": "warn",
+    // "no-undef": "off",
+    "react-native/no-inline-styles": "off",
+    "no-shadow": "off",
+    "newline-before-return": "warn",
+    "no-unreachable": "warn",
+    // eslint-import-order-plugin
+    "sort-imports": ["warn", { ignoreCase: true, ignoreDeclarationSort: true }],
+    "import/order": [
+      "warn",
+      {
+        groups: [["external", "builtin"], "internal", ["sibling", "parent"], "index"],
+        pathGroups: [
+          {
+            pattern: "@(react|react-native)",
+            group: "external",
+            position: "before"
+          },
+          {
+            pattern: "src/**",
+            group: "internal",
+            position: "after"
+          }
+        ],
+        pathGroupsExcludedImportTypes: ["internal", "react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true
+        }
+      }
+    ],
+    "import/newline-after-import": [
+      "error",
+      {
+        count: 1
+      }
+    ]
+  }
+}
