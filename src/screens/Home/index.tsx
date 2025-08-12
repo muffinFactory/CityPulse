ba8be25f75d5f3d73edc8937c1e00487eaa90e61
muffinFactory <muhharismaulana@gmail.com>
2025-08-12 22:49:11 +0700

    feat: HomeScreen init

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,26 @@
 import { FC } from "react"
-import { Text, View } from "react-native"
+import { View } from "react-native"
 
+import { BaseText } from "src/components/BaseText"
 import BaseScreen from "src/components/Layout/BaseScreen"
+import useAppTranslation from "src/hooks/useAppTranslation"
+import { useUserInfo } from "src/hooks/useUserInfo"
 import { HomeNavigationScreen } from "src/lib/routes/type"
+import { capitalize } from "src/util/string"
+
+import styles from "./styles"
 
 const HomeScreen: FC<HomeNavigationScreen<"Home">> = () => {
+  const { t } = useAppTranslation()
+  const userInfo = useUserInfo()
+
   return (
-    <BaseScreen style={{ justifyContent: "center", alignItems: "center" }} fullscreen={true}>
-      <View>
-        <Text>HomeScreen</Text>
+    <BaseScreen fullscreen={true}>
+      <View style={styles.header}>
+        <BaseText style={styles.headerText}>{`${capitalize(t("welcome"))}, ${userInfo?.name}`}</BaseText>
       </View>
+      {/* TODO: Insert TextBox search Input with eclipse like container */}
+      {/* TODO: Flatlist for EventCard */}
     </BaseScreen>
   )
 }
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+const HomeScreenStyles = StyleSheet.create({
+  header: {
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  headerText: {
+    fontSize: 18,
+    lineHeight: 20
+  }
+})
+
+export default HomeScreenStyles
