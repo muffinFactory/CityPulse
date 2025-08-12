b83cf1671a1438001afe0dd35fc7a839f90b24a8
muffinFactory <muhharismaulana@gmail.com>
2025-08-12 15:59:10 +0700

    fix(dev): module resolver & react-native-config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-};
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        alias: {
+          src: "./src"
+        },
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+      }
+    ],
+    "react-native-reanimated/plugin"
+  ]
+}
