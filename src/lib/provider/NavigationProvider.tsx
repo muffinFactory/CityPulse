a0463723581f5ab785ee9dd057ef7e44539742b4
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 08:53:22 +0700

    feat: goToAuth action

--- src/lib/provider/NavigationProvider.tsx
+++ src/lib/provider/NavigationProvider.tsx
@@ -1,18 +1,27 @@
-import React, { createContext, ReactNode } from "react"
+import React, { createContext, ReactNode, useCallback } from "react"
 
 import { NavigationContainer } from "@react-navigation/native"
+
+import useAppNavigation from "src/hooks/navigation/useAppNavigation"
 
 // type PossibleScreen = keyof HomeNavigatorParams
 
 export interface NavigationContextType {
   // TODO: if we want to add universal nav (404, error, etc, insert here)
+  goToAuth: () => void
 }
 
-export const NavigationContext = createContext<NavigationContextType>({})
+export const NavigationContext = createContext<NavigationContextType>({ goToAuth: () => {} })
 
 const NavProvider: React.FC<NavigationProviderProps> = ({ children }) => {
+  const navigation = useAppNavigation()
+
   // TODO: Insert universal navigation here
-  return <NavigationContext.Provider value={{}}>{children}</NavigationContext.Provider>
+  const goToAuth = useCallback(() => {
+    navigation.navigate("AuthNavigation")
+  }, [navigation])
+
+  return <NavigationContext.Provider value={{ goToAuth }}>{children}</NavigationContext.Provider>
 }
 
 type NavigationProviderProps = {
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -2,16 +2,18 @@ import { FC } from "react"
 
 import BaseScreen from "src/components/Layout/BaseScreen"
 import PromptAuthSection from "src/Fragment/PromptAuthSection"
+import useAppNavigationContext from "src/hooks/navigation/useAppNavigationContext"
 import { useUserInfo } from "src/hooks/useUserInfo"
 import { HomeNavigationScreen } from "src/lib/routes/type"
 
 const ProfileScreen: FC<HomeNavigationScreen<"Profile">> = ({}) => {
   const userInfo = useUserInfo()
+  const { goToAuth } = useAppNavigationContext()
 
   if (userInfo.isGuest)
     return (
       <BaseScreen fullscreen style={{ justifyContent: "center", alignItems: "center" }}>
-        <PromptAuthSection />
+        <PromptAuthSection onLogin={goToAuth} />
       </BaseScreen>
     )
 
