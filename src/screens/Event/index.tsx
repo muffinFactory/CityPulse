ae8b4126f3fabf5c745d3e419cad6f20e31626ec
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 15:36:42 +0700

    feat: Event Screen fav interaction

--- src/screens/Event/index.tsx
+++ src/screens/Event/index.tsx
@@ -4,14 +4,21 @@ import { StyleSheet, View } from "react-native"
 import moment from "moment"
 
 import { BaseText } from "src/components/BaseText"
+import AppButton from "src/components/Button"
 import BaseScreen from "src/components/Layout/BaseScreen"
 import ScreenHeader from "src/components/ScreenHeader"
+import { useUserInfo } from "src/hooks/useUserInfo"
 import { AppScreen } from "src/lib/routes/type"
 
 import NotFoundPage from "../NotFound"
 
 const EventScreen: FC<AppScreen<"Event">> = ({ route }) => {
   const locationEvent = route.params.event
+  const userInfo = useUserInfo()
+  // const insets = useA
+
+  const isFavorited = userInfo.favorite_events_ids?.includes(locationEvent?.id)
+  const setFavorite = (_item: any) => {} // TODO: setFavorite hook
 
   if (!locationEvent) {
     return <NotFoundPage />
@@ -26,6 +33,8 @@ const EventScreen: FC<AppScreen<"Event">> = ({ route }) => {
           {moment(locationEvent.dates?.start.dateTime).format("DD - MMMM - YYYY")}
         </BaseText>
       </View>
+
+      <View style={{ marginBottom: 20, borderBottomWidth: 3 }} />
       <View style={{ height: 300, width: "100%", backgroundColor: "black" }}>
         {/* TODO mapView */}
         {/* <MapView
@@ -44,6 +53,15 @@ const EventScreen: FC<AppScreen<"Event">> = ({ route }) => {
 						}}
 					/>
 				</MapView> */}
+      </View>
+
+      <View style={{ marginTop: 20, borderTopWidth: 3, padding: 20 }}>
+        <AppButton
+          text={isFavorited ? "Remove from Favorites" : "Add as Favorite"}
+          style={{ width: "50%" }}
+          onPress={() => setFavorite(locationEvent)}
+        />
+        {/* TODO isFavorited */}
       </View>
     </BaseScreen>
   )
