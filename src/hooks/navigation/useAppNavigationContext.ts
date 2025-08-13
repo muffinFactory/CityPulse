ba62007d1536667a98232ab6c52aacdd5213ab40
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 08:52:27 +0700

    fix: navigation context

--- src/hooks/navigation/useAppNavigationContext.ts
+++ src/hooks/navigation/useAppNavigationContext.ts
@@ -1,14 +1,12 @@
 import { useContext } from "react"
 
-import { NavigationContext } from "@react-navigation/native"
-
-import type { NavigationContextType } from "src/lib/provider/NavigationProvider"
+import { NavigationContext, type NavigationContextType } from "src/lib/provider/NavigationProvider"
 
 /**
  * Navigation hook to access universal screens (404, error, etc)
  */
 const useAppNavigationContext = () => {
-  const context = useContext(NavigationContext)
+  const context = useContext<NavigationContextType>(NavigationContext)
 
   if (!context) {
     throw new Error("useNavigationContext must be used within a NavigationProvider")
