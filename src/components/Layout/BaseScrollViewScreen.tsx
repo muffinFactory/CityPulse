32a78902b6af950a3e4985a774c25a71ed2135e4
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 09:29:58 +0700

    feat: extend BaseScrollViewScreen props

--- src/components/Layout/BaseScrollViewScreen.tsx
+++ src/components/Layout/BaseScrollViewScreen.tsx
@@ -1,26 +1,31 @@
 import React from "react"
-import { ScrollView, StyleProp, StyleSheet, View, ViewStyle } from "react-native"
+import { ScrollView, ScrollViewProps, StyleProp, StyleSheet, View, ViewStyle } from "react-native"
 
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { useBottomWindowPadding } from "src/hooks/layout/useBottomWindowPadding"
 import { useAppTheme } from "src/hooks/useAppTheming"
 
-type Props = {
+type Props = ScrollViewProps & {
   style?: StyleProp<ViewStyle>
+  containerStyle?: StyleProp<ViewStyle>
   children?: React.ReactNode
   fullscreen?: boolean
 }
 
-const BaseScrollViewScreen = ({ style, children, fullscreen }: Props) => {
+const BaseScrollViewScreen = (props: Props) => {
+  const { style, children, fullscreen, containerStyle, ...scrollViewProps } = props
   const bottomSpace = useBottomWindowPadding()
   const insets = useSafeAreaInsets()
   const Theme = useAppTheme()
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, containerStyle]}>
       {!fullscreen && <View style={{ height: insets.top, width: "100%", backgroundColor: Theme.statusBarColor }} />}
-      <ScrollView style={[styles.container, { paddingBottom: bottomSpace, backgroundColor: Theme.background }, style]}>
+      <ScrollView
+        style={[styles.container, { paddingBottom: bottomSpace, backgroundColor: Theme.background }, style]}
+        {...scrollViewProps}
+      >
         {children}
       </ScrollView>
     </View>
