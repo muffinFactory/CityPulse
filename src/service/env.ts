adaae3386d43fdda5a1607777408754ab622f539
muffinFactory <muhharismaulana@gmail.com>
2025-08-13 08:52:06 +0700

    chore(dev): axios & config setup

---
+++ src/service/env.ts
@@ -0,0 +1,9 @@
+import Config from "react-native-config"
+
+const API_KEY_DISCOVERY = Config.API_KEY_DISCOVERY
+
+const env = {
+  API_KEY_DISCOVERY // Note: we should not store API keys here
+}
+
+export default env
